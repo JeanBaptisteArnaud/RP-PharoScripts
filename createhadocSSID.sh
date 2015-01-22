422c63b954d8d7f9c6d9401fbead28ec0b7ed263
jeanbaptistearnaud <jbaptiste.arnaud@gmail.com>
2015-01-22 13:21:26 +0000

    change ip range and erased file

--- createhadocSSID.sh
+++ createhadocSSID.sh
@@ -3,13 +3,13 @@ sudo apt-get update
 sudo apt-get install hostapd udhcpd -y
 
 sudo echo "start 192.168.0.2 # This is the range of IPs that the hotspot will give to client devices.
-end 192.168.0.20
+end 192.168.0.200
 interface wlan0 # The device udhcp listens on.
 remaining yes
 opt dns 8.8.8.8 4.2.2.2 # The DNS servers client devices will use.
 opt subnet 255.255.255.0
 opt router 192.168.0.1 # The Pi's IP address on wlan0 which we will set up shortly.
-opt lease 864000 # 10 day DHCP lease time in seconds" >>  /etc/udhcpd.conf
+opt lease 864000 # 10 day DHCP lease time in seconds" >  /etc/udhcpd.conf
 
 
 sudo echo "
@@ -25,4 +25,4 @@ wpa=2
 wpa_passphrase=somepassword
 wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
-rsn_pairwise=CCMP" >> /etc/hostapd/hostapd.conf
+rsn_pairwise=CCMP" > /etc/hostapd/hostapd.conf
